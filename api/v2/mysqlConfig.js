1c4a35238f4cfe49bc24c4a967b4fe7f88b2cf68
r-console <rameshsolaimalai878@gmail.com>
2023-02-25 10:36:30 +0530

    s3 config and rds changed 2023

--- api/v2/mysqlConfig.js
+++ api/v2/mysqlConfig.js
@@ -2,8 +2,12 @@ const mysql = require("mysql")
 
 const pool = mysql.createPool({
   connectionLimit: 100,
-  host: "scs-appservice-database.cuies1xmg40c.ap-south-1.rds.amazonaws.com",
-  user: "scs2021admin",
+  // host: "scs-appservice-database.cuies1xmg40c.ap-south-1.rds.amazonaws.com",
+  // user: "scs2021admin",
+
+  // new aws rds connection 2023 feb
+  host: "scsbillsdb2.c4cket3gy2ao.ap-south-1.rds.amazonaws.com",
+  user: "scs2023admin",
   password: "nodejs#$#878",
   database: "scsappdbservices",
 
